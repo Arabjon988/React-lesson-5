00c7b9164a2dc8a2eb56e39c096f8d4feeffd3ab
Arabjon9899 <arabkhon3133@gmail.com>
2021-08-10 20:48:00 +0500

    bla

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
 import './App.css';
-import image from './Image/image.jpg'
-
+import photo from './Image/photo.jpg'
 function App() {
   return (
     <div className="App">
       <div className="videoBar">
         <div className="videoSection">
-          <img src={image} alt="logo" className="image__video" />
+          <img src={photo} alt="logo" className="image__video" />
         </div>
         <div className="videoCollect">
           <div className="video__collection">
-
             <h3>Recommended</h3>
-
-            <ul className="videoCollection">
-              <li className="videoCollection__item"><img src={image} className="videoItem" /></li>
-              <li className="videoCollection__item"><img src={image} className="videoItem" /></li>
-              <li className="videoCollection__item"><img src={image} className="videoItem" /></li>
-              <li className="videoCollection__item"><img src={image} className="videoItem" /></li>
-            </ul>
-            <h3>Trending</h3>
-
-            <ul className="videoCollection">
-              <li className="videoCollection__item"><img src={image} className="videoItem" /></li>
-              <li className="videoCollection__item"><img src={image} className="videoItem" /></li>
-              <li className="videoCollection__item"><img src={image} className="videoItem" /></li>
-              <li className="videoCollection__item"><img src={image} className="videoItem" /></li>
-            </ul>
-
           </div>
           <div className="video__info"></div>
         </div>
